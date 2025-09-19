824309ef785dc0563f175aba22534bb0afebc59b
anand <166482476+sarathmathii@users.noreply.github.com>
2025-09-19 15:13:38 +0530

    layout

---
+++ .history/frontend/vite-project/src/pages/Dashboard_20250919151233.jsx
@@ -0,0 +1,24 @@
+
+
+import "./Dashboard.css";
+ 
+export default function Dashboard() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+return (
+    <div className="dashboard-layout">
+
+      <div className="dashboard-body">
+        {/* Sidebar */}
+
+        {/* Main Content */}
+        <main className="dashboard-main">
+          <div className="dashboard-card">
+            <h4>Welcome Back {user?.name} !</h4>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+ 
