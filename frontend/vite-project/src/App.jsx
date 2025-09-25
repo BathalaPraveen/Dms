82689044ee54157a6521672b664bcd1eec31e225
Bathala Praveen <bathala.praveen@ardhas.com>
2025-09-25 16:43:46 +0530

    Update App.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -10,6 +10,7 @@ import EmployeeAdd from "./employee/EmployeeAdd";
 import "./i18n";
 import PrivateRoute from "./components/PrivateRoute";
 import { ThemeProvider } from "./contexts/ThemeContext";
+
 function App() {
   return (
     <ThemeProvider>
